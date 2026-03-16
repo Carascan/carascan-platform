13bfd71ba16f7a32c567c2c18073d6a751cc73ea
Nathan Conolan <nathan@conolanprojects.com.au>
2026-03-17 03:11:29 +1030

    Fix Next.js 16 setup page params handling

--- app/setup/[token]/page.tsx
+++ app/setup/[token]/page.tsx
@@ -1,5 +1,11 @@
-import SetupClient from "./setupClient";
+import SetupClient from "./SetupClient";
 
-export default function SetupPage({ params }: { params: { token: string } }) {
-  return <SetupClient token={params.token} />;
+export default async function SetupPage({
+  params,
+}: {
+  params: Promise<{ token: string }>;
+}) {
+  const { token } = await params;
+
+  return <SetupClient token={token} />;
 }
