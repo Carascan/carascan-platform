2b36458257df997be2b345498f1108bb3812aab1
Nathan Conolan <nathan@conolanprojects.com.au>
2026-03-29 21:11:16 +1030

    Restore PlatePreviewSection component after accidental overwrite

--- components/home/PlatePreviewSection.tsx
+++ components/home/PlatePreviewSection.tsx
@@ -1,31 +1,7 @@
-import PlatePreviewGenerator from "@/components/home/PlatePreviewGenerator";
-import SectionHeader from "@/components/home/SectionHeader";
+"use client";
+
+import PlatePreviewGenerator from "./PlatePreviewGenerator";
 
 export default function PlatePreviewSection() {
-  return (
-    <section
-      id="preview"
-      style={{
-        padding: "88px 20px",
-        background: "#ffffff",
-      }}
-    >
-      <div
-        style={{
-          maxWidth: 1200,
-          margin: "0 auto",
-        }}
-      >
-        <SectionHeader
-          eyebrow="Plate preview"
-          title="Preview your Carascan plate"
-          description="This section is split out so the plate preview can be edited independently and later connected to the locked manufacturing layout."
-        />
-
-        <div style={{ marginTop: 32 }}>
-          <PlatePreviewGenerator />
-        </div>
-      </div>
-    </section>
-  );
+  return <PlatePreviewGenerator />;
 }
